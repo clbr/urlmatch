c042617ba9faec5d7ff5c3fdee2abde55b7513f9
Lauri Kasanen <curaga@operamail.com>
2013-09-20 18:23:03 +0300

    More tests

--- test/simple.c
+++ test/simple.c
@@ -22,5 +22,21 @@ int main() {
 	test("*", "*g****", 1);
 	test("*", "*", 1);
 
+	test("*foo", "foo", 1);
+	test("*foo", "foofoo", 1);
+	test("*foo", "ofoo", 1);
+	test("*foo", "fo", 0);
+	test("*foo", "oof", 0);
+
+	test("*foo*", "foo", 1);
+	test("*foo*", "bfoob", 1);
+	test("*foo*", "*foo*", 1);
+	test("*foo*", "foishbar", 0);
+	test("*foo*", "foko", 0);
+	test("*foo*", "fokooooooooooooofoo", 1);
+
+	//test("*f*o*o*", "foo", 1);
+
+
 	return 0;
 }
