50c5f08d89d8ccbb76cf71a1b212b540c2aaf89c
Lauri Kasanen <curaga@operamail.com>
2013-09-20 18:52:57 +0300

    More

--- bench/bench.c
+++ bench/bench.c
@@ -5,21 +5,49 @@
 #include <lrtypes.h>
 #include "urlmatch.h"
 
+const u32 urls = 1000 * 1000;
+const u32 rules = 1500;
+
+static void gen() {
+
+}
+
+static void simple() {
+
+}
+
+static void opti() {
+
+}
+
 int main() {
 
 	srand(time(NULL));
 
+	printf("Generating %u urls and %u rules.\n", urls, rules);
 	gen();
 
 	struct timeval start, end;
+	u32 ms;
 
+	printf("Starting testing.\n");
 	gettimeofday(&start, NULL);
 	simple();
 	gettimeofday(&end, NULL);
 
+	ms = end.tv_sec - start.tv_sec;
+	ms += (end.tv_usec - start.tv_usec) * 1000;
+	printf("Simple backend took %u ms, or %u checks per second.\n",
+		ms, urls / (ms / 1000));
+
 	gettimeofday(&start, NULL);
 	opti();
 	gettimeofday(&end, NULL);
+
+	ms = end.tv_sec - start.tv_sec;
+	ms += (end.tv_usec - start.tv_usec) * 1000;
+	printf("Simple backend took %u ms, or %u checks per second.\n",
+		ms, urls / (ms / 1000));
 
 	return 0;
 }
