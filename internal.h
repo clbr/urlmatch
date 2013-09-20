df7591e26bf5f1a1edd14cafa0c0869bf94bdaf0
Lauri Kasanen <curaga@operamail.com>
2013-09-20 17:22:07 +0300

    lol guards

--- internal.h
+++ internal.h
@@ -15,8 +15,8 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifndef URLMATCH_H
-#define URLMATCH_H
+#ifndef INTERNAL_H
+#define INTERNAL_H
 
 #include <lrtypes.h>
 
