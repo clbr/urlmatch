76c5e90ce3970677812ebccfe3226f70a8d8f3c2
Lauri Kasanen <curaga@operamail.com>
2013-09-21 15:00:53 +0300

    Add opti stubs

---
+++ opti.c
@@ -0,0 +1,29 @@
+/*
+    liburlmatch - a fast URL matcher
+    Copyright (C) 2013 Lauri Kasanen
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, version 3 of the License.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "internal.h"
+#include "urlmatch.h"
+
+urlctx *url_init_file(const char file[]);
+urlctx *url_init(const char contents[]);
+
+int url_save_optimized(const urlctx *ctx, const char file[]);
+
+int url_match(const urlctx *ctx, const char haystack[]);
+
+void url_free(urlctx *ctx);
+
