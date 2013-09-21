9a4e0b6203f67639503594381c7dba3b40753345
Lauri Kasanen <curaga@operamail.com>
2013-09-21 17:20:18 +0300

    Clarify alloc test urls

--- test/allocfree.c
+++ test/allocfree.c
@@ -2,7 +2,13 @@
 
 int main() {
 
-	urlctx *ctx = url_init("http://moi\nhttps://katti\nmoido\nhttp://google*");
+	const char pat[] =
+		"http://moi\n"
+		"https://katti\n"
+		"moido\n"
+		"http://google*";
+
+	urlctx *ctx = url_init(pat);
 	url_free(ctx);
 
 	return 0;
