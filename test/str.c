364f2aa42bfd155af037c1158a38982080e23b4e
Lauri Kasanen <curaga@operamail.com>
2014-01-05 18:39:21 +0200

    Add suffixcmp tests

--- test/str.c
+++ test/str.c
@@ -1,7 +1,7 @@
 #include "test.h"
 #include "internal.h"
 
-void test(const char hay[], const char needle[], const u32 pos) {
+static void test(const char hay[], const char needle[], const u32 pos) {
 
 	const char * const ptr = strrstr(hay, needle);
 	const char * const exists = strstr(hay, needle);
@@ -18,6 +18,14 @@ void test(const char hay[], const char needle[], const u32 pos) {
 			needle, hay, pos, tmp);
 }
 
+static void suf(const char one[], const char two[], const u32 nomatch) {
+	const u32 res = suffixcmp(one, two);
+
+	if (res != nomatch)
+		fail("suffixcmp %s %s got %u expected %u\n",
+			one, two, res, nomatch);
+}
+
 int main() {
 
 	test("foo", "bar", 0);
@@ -27,6 +35,16 @@ int main() {
 	test("foo", "foofoo", 3);
 	test("foo", "barfoo", 3);
 	test("foo", "foofoobar", 3);
+
+	suf("aa", "ab", 1);
+	suf("bb", "ab", 1);
+	suf("a", "ab", 1);
+	suf("aa", "b", 1);
+
+	suf("aa", "aa", 0);
+	suf("bb", "bb", 0);
+	suf("aa", "a", 0);
+	suf("a", "aa", 0);
 
 	return 0;
 }
