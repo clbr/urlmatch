e813ed1bf12ef8220e01ce0ee6993892f143bc75
Lauri Kasanen <curaga@operamail.com>
2014-01-05 17:27:14 +0200

    Add a second test to allocfree

--- test/allocfree.c
+++ test/allocfree.c
@@ -12,5 +12,15 @@ int main() {
 	urlctx *ctx = url_init(pat);
 	url_free(ctx);
 
+	const char pat2[] =
+		"http://moi\n"
+		"https://katti\n"
+		"moido\n"
+		"http://hoi\n"
+		"http://google*\n\n\n";
+
+	ctx = url_init(pat2);
+	url_free(ctx);
+
 	return 0;
 }
